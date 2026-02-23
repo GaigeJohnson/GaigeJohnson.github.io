d50e2ed7341f075ecb2aa1e257a7c4bdb36b1410
Gaige <gmjohnson2798@gmail.com>
2026-02-22 23:34:52 -0500

    adding dark mode toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,16 @@
 const form = document.querySelector("form");
+const toggleBtn = document.querySelector("#dark-mode-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", function() {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "Switch to Light Mode";
+    } else {
+        toggleBtn.textContent = "Switch to Dark Mode";
+    }
+});
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
